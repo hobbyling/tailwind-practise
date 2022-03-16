88e48076f57794f43dac036f386822ad31c35eea
Hobby <g99949@hotmail.com>
2022-03-16 10:42:01 +0800

    客製化樣式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -21,5 +22,28 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    plugin(function ({ addBase, addComponents, addUtilities, theme }) {
+      addBase({
+        'h1': {
+          fontSize: theme('fontSize.2xl'),
+        },
+        'h2': {
+          fontSize: theme('fontSize.xl'),
+        },
+      })
+      addComponents({
+        '.card': {
+          backgroundColor: theme('colors.white'),
+          borderRadius: theme('borderRadius.lg'),
+          padding: theme('spacing.6'),
+          boxShadow: theme('boxShadow.xl'),
+        }
+      })
+      addUtilities({
+        '.content-auto': {
+          contentVisibility: 'auto',
+        }
+      })
+    })
   ],
 }
